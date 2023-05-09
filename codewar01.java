e08f50f6181b7b9b92cf77363a41077d5ac526cd
Eliel Dias Matos <106202766+Z0r0pa@users.noreply.github.com>
2023-05-09 00:47:34 -0300

    Update codewar01.java

--- codewar01.java
+++ codewar01.java
@@ -1,3 +1,10 @@
+PROBLEMA ( ******Você estava acampando com seus amigos longe de casa, mas quando chega a hora de voltar,
+você percebe que o combustível está acabando e a bomba mais próxima fica a 50 milhas de distância! 
+    Você sabe que, em média, seu carro roda cerca de 25 milhas por galão. Restam 2 galões.
+Considerando esses fatores, escreva uma função que diga se é possível chegar à bomba ou não.
+A função deve retornar true se for possível e false se não.******)
+
+
 public class codewar01 {
     public static void main(String[]args){
         //A bomba mais próxima fica a 50 milhas
